f20f38a1e8279c84d62fe6b53614fa9d4b97cabb
israeldatascience <123495355+israeldatascience@users.noreply.github.com>
2023-01-24 15:16:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,4 @@ import streamlit as st
 """
 # Dados básicos do IG:
 """
- 
-    
-# Read in the data from the Google Sheets CSV file
-data = pd.read_csv("https://docs.google.com/spreadsheets/d/1oq0SyMfRkEGyIP8SpMZWfjKzpn7B_69k59o6l2PqJpE/edit?usp=sharing")
-
-# Create a Streamlit app
-st.title("Google Sheets Data")
-
-# Display the data in a table
-st.dataframe(data)
+print("dados:")
