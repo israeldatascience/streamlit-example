9b73561f637b970fa403c92def3934446bde5ed0
israeldatascience <123495355+israeldatascience@users.noreply.github.com>
2023-01-24 15:30:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,13 @@ import streamlit as st
 """
 st.write(""" Perfect""")
 
-# 
+# select a number:
 number = st.slider("Pick a number", 0,100)
 
 # Read CSV file from GitHub repository
 df = pd.read_csv("dados1.csv")
 
 # Display data in a Streamlit dataframe
-column = df['SETEMBRO']*number
-st.dataframe(column)
+ 
+column = df.loc[df.index >= 3, 'SETEMBRO']
+st.dataframe(column*number)
