dbc0ef2a3ad52adea43e46ae96c04ed470a43806
israeldatascience <123495355+israeldatascience@users.noreply.github.com>
2023-01-24 16:01:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,18 @@ import streamlit as st
 df = pd.read_csv("dados1.csv")
 df2 = df
 df3 = df
+
+st.write("""LAST MEASURE - YOUR RESULTS""")
+st.dataframe(df['16/01/2023']) 
+st.dataframe(df)
+
+# select a number:
+n = st.slider("Choose a multiplier of your growth - DESIRED GROWTH:", 2,100)
 # MULTIPLICATE THE LAST DATE AND ALL DF
 df2['16/01/2023'] = df2['16/01/2023'] * n
 column = df2['16/01/2023'] 
 df3.iloc[:,1:] = df3.iloc[:,1:] * n
 
-st.write("""LAST MEASURE - YOUR RESULTS""")
-st.dataframe(column) 
-st.dataframe(df)
-
-# select a number:
-n = st.slider("Choose a multiplier of your growth - DESIRED GROWTH:", 2,100)
 st.write("""DESIRED GROWTH: """)
 st.dataframe(df3)
+st.dataframe(column)
