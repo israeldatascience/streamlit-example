5786d62530f780a6a41d4bbe00cd7f9a95a655d9
israeldatascience <123495355+israeldatascience@users.noreply.github.com>
2023-01-24 15:32:49 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,14 @@ import streamlit as st
 st.write(""" Perfect""")
 
 # select a number:
-number = st.slider("Pick a number", 0,100)
+n = st.slider("Pick a number", 0,100)
 
 # Read CSV file from GitHub repository
 df = pd.read_csv("dados1.csv")
 
 # Display data in a Streamlit dataframe
  
-column = df.loc[df.index >= 3, 'SETEMBRO']
-st.dataframe(column*number)
+column = df.loc[df.index >= 1, 'SETEMBRO']
+column = column.applymap(lambda x: x*n)
+
+st.dataframe(column)
