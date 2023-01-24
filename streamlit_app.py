9996ccdeebf66e1a5c1013a89db91fb77efcc825
israeldatascience <123495355+israeldatascience@users.noreply.github.com>
2023-01-24 15:58:54 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,19 +7,21 @@ import streamlit as st
 """
 # Dados básicos do IG:
 """
-st.write(""" Perfect""")
-
-# select a number:
-n = st.slider("Pick a number", 0,100)
 
 # Read CSV file from GitHub repository
 df = pd.read_csv("dados1.csv")
+df2 = df
+df3 = df
+# MULTIPLICATE THE LAST DATE AND ALL DF
+df2.['16/01/2023'] = df2.['16/01/2023'] * n
+column = df2.['16/01/2023'] 
+df3.iloc[:,1:] = df3.iloc[:,1:] * n
 
- 
- 
-df.loc[df.index >= 3,'16/01/2023'] = df.loc[df.index >= 3,'16/01/2023'] * n
-column = df.loc[df.index >= 1, '16/01/2023'] 
-
-st.dataframe(column)
+st.write("""LAST MEASURE - YOUR RESULTS""")
+st.dataframe(column) 
 st.dataframe(df)
-st.write(n)
+
+# select a number:
+n = st.slider("Choose a multiplier of your growth - DESIRED GROWTH:", 2,100)
+st.write("""DESIRED GROWTH: """)
+st.dataframe(df3)
