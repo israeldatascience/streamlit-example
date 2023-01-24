d15bc750054d4f3587bfe329a02f4c45223a4d69
israeldatascience <123495355+israeldatascience@users.noreply.github.com>
2023-01-24 15:41:11 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,8 @@ n = st.slider("Pick a number", 0,100)
 df = pd.read_csv("dados1.csv")
 
 # Display data in a Streamlit dataframe
-df = df.applymap(lambda x: x*n)
+#df = df.applymap(lambda x: x*n)
+df= df+3
  
 column = df.loc[df.index >= 1, 'SETEMBRO'] 
 
